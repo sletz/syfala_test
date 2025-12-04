4922cc540b5422836d25251f4ac626758c9f9075
AquaEBM <AquaEBM@gmail.com>
2025-12-04 14:53:12 +0100

    oops forgot to set broadcast

--- syfala_jack/examples/client.rs
+++ syfala_jack/examples/client.rs
@@ -1,5 +1,6 @@
 fn main() -> std::io::Result<core::convert::Infallible> {
     let socket = std::net::UdpSocket::bind("0.0.0.0:6910")?;
+    socket.set_broadcast(true)?;
 
     syfala_jack::client::start(
         &socket,
